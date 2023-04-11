369bca5d32ca381eebdfea85712b2436921b78ab
aleksandraleszczynska <a.leszczy@interia.pl>
2023-04-11 10:32:50 +0200

    .

--- PAKIET_2/LEKCJA_5/ZAD5.5/zad5.5.cpp
+++ PAKIET_2/LEKCJA_5/ZAD5.5/zad5.5.cpp
@@ -3,3 +3,33 @@
 #include <iostream>
 
 using namespace std;
+
+int main()
+{
+    int rowCount = 2;
+    int columnCount = 2;
+    int tablica[rowCount][columnCount];
+
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < columnCount; j++)
+        {
+            cout << "Podaj liczbe do tablicy" << endl;
+            cin >> tablica[i][j];
+        }
+    }
+
+    for (int i = 0; i < rowCount; i++)
+    {
+        int suma = 0;
+
+        for (int j = 0; j < columnCount; j++)
+        {
+            cout << tablica[i][j] << " ";
+            suma = suma + tablica[i][j];
+        }
+
+        cout << suma << endl;
+    }
+    return 0;
+}
