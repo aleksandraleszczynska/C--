73b94e0454a277bbb0f8643cce00b33aaf800bb9
aleksandraleszczynska <a.leszczy@interia.pl>
2023-06-02 22:02:37 +0200

    .

--- PAKIET_2/LEKCJA_7/ZAD7.6/zad7.6.cpp
+++ PAKIET_2/LEKCJA_7/ZAD7.6/zad7.6.cpp
@@ -7,19 +7,41 @@ using namespace std;
 
 bool CzyCyfryWWektorzeTworzaPalindrom(vector<int> liczby)
 {
+    vector<char> nowywektor;
+
     for (int i = 0; i < liczby.size(); i++)
     {
-        if (liczby[i] != liczby[liczby.size() - 1 - i])
+        int liczba = liczby[i];
+
+        if (liczba == 0)
+        {
+            nowywektor.push_back('0');
+        }
+        else
+        {
+            while (liczba > 0)
+            {
+                int cyfra = liczba % 10;
+                nowywektor.push_back(cyfra + '0');
+                liczba = liczba / 10;
+            }
+        }
+    }
+
+    for (int i = 0; i < nowywektor.size(); i++)
+    {
+        if (nowywektor[i] != nowywektor[nowywektor.size() - i - 1])
         {
             return false;
         }
     }
+
     return true;
 }
 
 int main()
 {
-    vector<int> liczby = {1, 2, 3, 2, 1};
+    vector<int> liczby = {1, 24, 3, 42, 1};
 
     cout << CzyCyfryWWektorzeTworzaPalindrom(liczby);
 
