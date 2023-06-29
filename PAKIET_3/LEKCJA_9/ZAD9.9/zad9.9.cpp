cf9f98f4c40691090acecd0933da880a060eccf5
aleksandraleszczynska <a.leszczy@interia.pl>
2023-06-29 14:18:47 +0200

    .

---
+++ PAKIET_3/LEKCJA_9/ZAD9.9/zad9.9.cpp
@@ -0,0 +1,34 @@
+//Napisz program, który sprawdzi czy podany wzorzec zawiera się w tekście.
+
+#include <iostream>
+
+using namespace std;
+
+bool CzyWzorzecZawieraSieWTekscie(string tekst, string wzorzec)
+{
+    int licznik = wzorzec.length();
+
+    for (int i = 0; i < tekst.length(); i++)
+    {
+        if (tekst[i] == wzorzec[i])
+        {
+            licznik--;
+        }
+        else
+        {
+            licznik = wzorzec.length();
+            i = 0;
+        }
+    }
+    
+    return licznik == 0;
+}
+
+
+
+int main ()
+{
+    cout << CzyWzorzecZawieraSieWTekscie("LOOOLOLOTTT", "LOT");
+
+    return 0;
+}
