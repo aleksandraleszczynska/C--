dbf0a4d8b5f45cd48cc07ae15f918e5e3e019f6a
aleksandraleszczynska <a.leszczy@interia.pl>
2023-07-21 23:21:44 +0200

    .

--- PAKIET_3/LEKCJA_12/ZAD12.3/zad12.3.cpp
+++ PAKIET_3/LEKCJA_12/ZAD12.3/zad12.3.cpp
@@ -36,19 +36,19 @@ void quicksort(int tablica[], int lewyindex, int prawyindex, int pivotindex)
         pivotindex = granica;
     }
 
-    quicksort(tablica, lewyindex, granica - 1, pivotindex);
+    quicksort(tablica, lewyindex, granica - 1, granica - 1);
     quicksort(tablica, granica + 1, prawyindex, prawyindex);
 }
 
 int main()
 {
-    int tablica[5] = {3, 5, 1, 5, 1};
+    int tablica[7] = {1, 5, 2, 7, 1, 3, 4};
     int lewy = 0;
-    int prawy = 4;
-    int pivotindex = 2;
+    int prawy = 6;
+    int pivotindex = 3;
     quicksort(tablica, lewy, prawy, pivotindex);
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 7; i++)
     {
         cout << tablica[i] << " ";
     }
--- PAKIET_3/LEKCJA_12/ZAD12.4/zad12.4.cpp
+++ PAKIET_3/LEKCJA_12/ZAD12.4/zad12.4.cpp
@@ -1,1 +1,57 @@
 //Napisz program, który posortuje tablicę rosnąco za pomocą zmodyfikowanego algorytmu sortowania szybkiego, w którtym pivotem jest losowy indeks z przedziału od lewegoIndexu do prawego, wykonywanego podproblemu. Losowanie liczby z danego zakresu znajdziesz z użyciem wujka google 😃
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void quicksort(int tablica[], int lewyindex, int prawyindex)
+{
+    int licznikIndex = lewyindex;
+    int granica = lewyindex - 1;
+    int pivot = tablica[lewyindex + rand() % (prawyindex-lewyindex +1)];
+
+    if (lewyindex >= prawyindex)
+    {
+        return;
+    }
+
+    while (licznikIndex <= prawyindex)
+    {
+        if (tablica[licznikIndex] < pivot)
+        {
+            granica++;
+            if (granica != licznikIndex)
+            {
+                swap(tablica[granica], tablica[licznikIndex]);
+            }
+        }
+
+        licznikIndex++;
+    }
+
+    granica++;
+    if (granica != prawyindex)
+    {
+        swap(tablica[granica], tablica[pivot]);
+        pivot = granica;
+    }
+
+    quicksort(tablica, lewyindex, granica - 1);
+    quicksort(tablica, granica + 1, prawyindex);
+}
+
+int main()
+{
+    int tablica[7] = {1, 5, 2, 7, 1, 3, 4};
+    int lewy = 0;
+    int prawy = 6;
+    quicksort(tablica, lewy, prawy);
+
+    for (int i = 0; i < 7; i++)
+    {
+        cout << tablica[i] << " ";
+    }
+
+    return 0;
+}
