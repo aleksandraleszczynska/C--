63e9c051c05f7d7cd29179431aa1484798ee7d4c
aleksandraleszczynska <a.leszczy@interia.pl>
2023-08-06 21:29:27 +0200

    .

--- PAKIET_4/LEKCJA_13/ZAD13.11/zad13.11.cpp
+++ PAKIET_4/LEKCJA_13/ZAD13.11/zad13.11.cpp
@@ -8,3 +8,58 @@
 // Przykład: Tekst jawny: MARTA, klucz: TOR M+T = 77+20 = 97, 97-26=71, G A+O = 65+15 =80, P R+R = 82+18= 100, 100-26 = 74, J T+T = 84+20 = 104, 104-26=78, N A+O = 65+15 =80, P Szyfrogram: GPJNP
 //     W procesie deszyfrowania szyfrogram przekształcany jest na tekst jawny przy pomocy klucza poprzez odjęcie od kodu litery szyfrogramu numeru odpowiadającej jej litery klucza (jeżeli tak uzyskana wartość liczbowa będzie mniejsza od 65, należy ją powiększyć o 26) i odczytanie litery o otrzymanym kodzie.
 // b) W pliku sz.txt znajdują się zaszyfrowane słowa, a w pliku klucze2.txt znajdują się klucze deszyfrujące. W obu plikach wyrazy umieszczone są w osobnych wierszach. Odszyfruj słowa zawarte w pliku sz.txt, wynik zapisz do pliku wynik4b.txt. Wyraz zapisany w N-tym wierszu w pliku z wynikami powinien stanowić tekst jawny szyfrogramu znajdującego się w N-tym wierszu w pliku z szyfrogramami uzyskany za pomocą klucza zapisanego w N-tym wierszu pliku z kluczami.
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+string deszyfrowanie(string slowo, string klucz)
+{
+    string szyfrogram = "";
+    char litera;
+
+    for (int i = 0; i < slowo.length(); i = i + klucz.length())
+    {
+        for (int j = 0; j < klucz.length(); j++)
+        {
+            if (i + j >= slowo.length())
+            {
+                return szyfrogram;
+            }
+            else
+            {
+                litera = slowo[i + j] - (klucz[j] - 64);
+
+                if (litera < 65)
+                {
+                    litera = litera + 26;
+                }
+                szyfrogram = szyfrogram + litera;
+            }
+        }
+    }
+}
+
+int main()
+{
+    fstream plik1;
+    fstream plik2;
+    ofstream zapis;
+    string slowo;
+    string klucz;
+
+    plik1.open("../PLIKI/sz.txt");
+    plik2.open("../PLIKI/klucze2.txt");
+    zapis.open("../PLIKI/wynik4b.txt");
+
+    zapis << "b) ";
+
+    while (!plik1.eof())
+    {
+        plik1 >> slowo;
+        plik2 >> klucz;
+        zapis << deszyfrowanie(slowo, klucz) << endl;
+    }
+    zapis.close();
+    return 0;
+}
