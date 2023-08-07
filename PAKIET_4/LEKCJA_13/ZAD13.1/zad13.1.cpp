7edbaadcc169af9b4c6e52dbe9f5f3fc143ee9f3
aleksandraleszczynska <a.leszczy@interia.pl>
2023-08-07 23:11:33 +0200

    .

--- PAKIET_4/LEKCJA_13/ZAD13.1/zad13.1.cpp
+++ PAKIET_4/LEKCJA_13/ZAD13.1/zad13.1.cpp
@@ -1,21 +1,19 @@
-//Przedstaw swój pomysł na algorytm szyfrujący jakiś tekst. Za pomocą opisu lub kodu 😉 Musi być możliwość sprawdzenia, czy tekst pasuje do szyfrogramu.
+// Przedstaw swój pomysł na algorytm szyfrujący jakiś tekst. Za pomocą opisu lub kodu 😉 Musi być możliwość sprawdzenia, czy tekst pasuje do szyfrogramu.
 
-//Algorytm szyfrujący podmienia co drugą literę z tej samej pozycji od początku i końca wyrazu.
+// Algorytm szyfrujący podmienia co drugą literę z tej samej pozycji od początku i końca wyrazu.
 
 #include <iostream>
 using namespace std;
 
 string szyfrowanie(string napis)
 {
-    string szyfrogram = "";
-
-    for(int i = 0; i < napis.length()/2; i = i+2)
+    for (int i = 0; i < napis.length() / 2; i = i + 2)
     {
-        int temp = napis[i]; 
-        szyfrogram[i] = napis[napis.length() -1 -i];
-        szyfrogram[napis.length() - 1 - i] = temp;
+        char temp = napis[i];
+        napis[i] = napis[napis.length() - 1 - i];
+        napis[napis.length() - 1 - i] = temp;
     }
-return napis;
+    return napis;
 }
 
 int main()
